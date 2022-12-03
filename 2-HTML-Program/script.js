99747a62b64d5e711295a1c641bc167f44de8620
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-02 23:56:04 -0300

    Practicing math operations

--- 2-HTML-Program/script.js
+++ 2-HTML-Program/script.js
@@ -1,2 +1,13 @@
 alert("Christian has 30 years old");
 document.write("<h1>How old are you?</h1>");
+document.write("<hr>");
+document.write("<h2>How old are they?</h2>");
+
+document.write("<p>Juan was born in 2000</p>");
+document.write(2022-2000);
+
+document.write("<p>Pedro was born in 1990</p>");
+document.write(2022-1990);
+
+document.write("<p>ESteban was born in 1970</p>");
+document.write(2022-1970);
