f2b8556b4df02cba5a82afba9957ad10259b600d
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-03 00:55:21 -0300

    Creating variables

--- 2-HTML-Program/script.js
+++ 2-HTML-Program/script.js
@@ -1,13 +1,28 @@
+let year=2022;
+let edadPaola=19;
+let edadXimena=12;
+let edadAna=27;
+let name1="Paola";
+let name2="Ximena";
+let name3="Ana";
+let promedio=Math.round((edadPaola+edadXimena+edadAna)/3);
+
 alert("Christian has 30 years old");
 document.write("<h1>How old are you?</h1>");
 document.write("<hr>");
 document.write("<h2>How old are they?</h2>");
 
-document.write("<p>Juan was born in 2000</p>");
-document.write(2022-2000);
+document.write("Juan was born in 2000. " + "He has "+(year-2000) + " years old. <br> <br>");
 
-document.write("<p>Pedro was born in 1990</p>");
-document.write(2022-1990);
+document.write("Pedro was born in 1990. " + "He has "+(year-1990) + " years old. <br> <br>");
 
-document.write("<p>ESteban was born in 1970</p>");
-document.write(2022-1970);
+document.write("Esteban was born in 1970. " + "He has "+(year-1970) + " years old. <br> <br>");
+
+document.write("Paola was born in 2003. " + "She has "+ edadPaola + " years old. <br> <br>");
+
+document.write("Ximena was born in 2010. " + "She has "+ edadXimena + " years old. <br> <br>");
+
+document.write("Ana was born in 1995. " + "She has "+ edadAna + " years old. <br> <br>");
+
+document.write("The average age of " +name1+", "+name2+" y "+name3+ " is " + promedio + ".");
+
