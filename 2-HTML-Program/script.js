8a89d52b543d7cfc911832c831c29c7925da1751
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-03 23:41:37 -0300

    Creating functions

--- 2-HTML-Program/script.js
+++ 2-HTML-Program/script.js
@@ -7,22 +7,26 @@ let name2="Ximena";
 let name3="Ana";
 let promedio=Math.round((edadPaola+edadXimena+edadAna)/3);
 
+function print(elementToPrint){
+    document.write(elementToPrint);
+    document.write("<br><br><br>");
+};
+
 alert("Christian has 30 years old");
 document.write("<h1>How old are you?</h1>");
-document.write("<hr>");
 document.write("<h2>How old are they?</h2>");
 
-document.write("Juan was born in 2000. " + "He has "+(year-2000) + " years old. <br> <br>");
+print("Juan was born in 2000. " + "He has "+(year-2000) + " years old.");
 
-document.write("Pedro was born in 1990. " + "He has "+(year-1990) + " years old. <br> <br>");
+print("Pedro was born in 1990. " + "He has "+(year-1990) + " years old.");
 
-document.write("Esteban was born in 1970. " + "He has "+(year-1970) + " years old. <br> <br>");
+print("Esteban was born in 1970. " + "He has "+(year-1970) + " years old.");
 
-document.write("Paola was born in 2003. " + "She has "+ edadPaola + " years old. <br> <br>");
+print("Paola was born in 2003. " + "She has "+ edadPaola + " years old.");
 
-document.write("Ximena was born in 2010. " + "She has "+ edadXimena + " years old. <br> <br>");
+print("Ximena was born in 2010. " + "She has "+ edadXimena + " years old.");
 
-document.write("Ana was born in 1995. " + "She has "+ edadAna + " years old. <br> <br>");
+print("Ana was born in 1995. " + "She has "+ edadAna + " years old.");
 
-document.write("The average age of " +name1+", "+name2+" y "+name3+ " is " + promedio + ".");
+print("The average age of " +name1+", "+name2+" y "+name3+ " is " + promedio + ".");
 
---
+++ 4-Difference_calculation/script.js
@@ -0,0 +1,14 @@
+function print(num1, num2){
+    document.write("Our age difference is of " + (num1-num2));
+    document.write("<br><br>");
+};
+
+function bigText(msg){
+    document.write("<big> This is a string into of a big tag: "+msg+"</big>");
+};
+
+print(46, 31);
+print(65, 45);
+bigText("I am a string")
+
+
