5548dee3c4ac668431133e04caa7e6300faf8633
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-12-08 14:04:33 -0300

    Improving the body mass index program

--- 5-Body_mass_index/script.js
+++ 5-Body_mass_index/script.js
@@ -7,7 +7,7 @@ function printPhrase(phrase) {
 }
 
 function calculateBMI(height, weight) {
-    return Math.floor(weight/Math.pow(height, 2));
+    return (weight/Math.pow(height, 2)).toFixed(2);
 };
 
 let namePatient=prompt("Hello! What is your name?")
@@ -15,3 +15,10 @@ let weight=Number(prompt(`${namePatient} what is your weight?`));
 let height=Number(prompt(`What is your height?`));
 let bMICalculated=calculateBMI(height, weight);
 printPhrase("The body mass index calculated is: " + bMICalculated);
+if(bMICalculated<18.5){
+    printPhrase("<h2>You are underweight.</h2>");
+} else if(bMICalculated>=18.5&&bMICalculated<24.9){
+    printPhrase("<h2>You have a normal weight.</h2>");
+} else if(bMICalculated>24.9){
+    printPhrase("<h2>You are overweight.</h2>");
+}
